0fb8e6802a724e17d972989d16d440493d119d5b
이성찬 <>
2018-05-03 11:42:12 +0900

    소스 커밋

---
+++ src/components/Control.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  onPlus: PropTypes.func,
+  onSubtract: PropTypes.func,
+  onRandomizeColor: PropTypes.func
+};
+
+function createWarning(funcName) {
+  return () => console.warn(funcName + ' is not defined');
+}
+
+const defaultProps = {
+  onPlus: createWarning('onPlus'),
+  onSubtract: createWarning('onSubtract'),
+  onRandomizeColor: createWarning('onRandomizeColor')
+};
+
+class Control extends React.Component {
+  
+    render() {
+        return(
+            <div>
+                <button onClick={this.props.onPlus}>+</button>
+                <button onClick={this.props.onSubtract}>-</button>
+                <button onClick={this.props.onRandomizeColor}>Ramdomize Color</button>
+            </div>
+        );
+    }
+}
+Control.propTypes = propTypes;
+Control.defaultProps = defaultProps;
+export default Control;
